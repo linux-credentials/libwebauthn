c455aed5c3959373777b5589b24af6fdf9e1c3f2
Alfie Fresta <alfie.fresta@gmail.com>
2025-08-15 11:13:59 +0100

    GetAssertion IDL implementation

--- libwebauthn/examples/webauthn_preflight_hid.rs
+++ libwebauthn/examples/webauthn_preflight_hid.rs
@@ -11,8 +11,8 @@ use tokio::sync::broadcast::Receiver;
 use tracing_subscriber::{self, EnvFilter};
 
 use libwebauthn::ops::webauthn::{
-    Base64UrlString, GetAssertionRequest, GetAssertionResponse, MakeCredentialRequest,
-    ResidentKeyRequirement, UserVerificationRequirement,
+    Base64UrlString, GetAssertionRequest, GetAssertionRequestExtensions, GetAssertionResponse,
+    MakeCredentialRequest, ResidentKeyRequirement, UserVerificationRequirement,
 };
 use libwebauthn::pin::PinRequestReason;
 use libwebauthn::proto::ctap2::{
@@ -202,7 +202,7 @@ async fn get_assertion_call(
         hash: Vec::from(challenge),
         allow: allow_list,
         user_verification: UserVerificationRequirement::Discouraged,
-        extensions: None,
+        extensions: GetAssertionRequestExtensions::default(),
         timeout: TIMEOUT,
     };
 
